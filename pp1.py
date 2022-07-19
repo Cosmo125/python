ac6f2c29a772979563a16d0841f4e34fa39c3ba2
Cosmo125 <102827802+Cosmo125@users.noreply.github.com>
2022-07-19 15:42:55 -0500

    Funciones_para_gpio_rockpi4

--- pp1.py
+++ pp1.py
@@ -1,13 +1,18 @@
 import time
-import os
 
 def initpin(pinnum, mode):
     '''
     pinnum: pin number, eg. 172, 175 etc.
     mode: pin mode, valid values: in or out
     '''
-    with open('/sys/class/gpio/export' , 'w') as f:
-        f.write(str(pinnum))
+    try:
+        with open('/sys/class/gpio/export' , 'w') as f:
+            
+                f.write(str(pinnum))
+                
+    except OSError:
+        print("Ya se ejecuto correctamente")
+ 
 
     with open('/sys/class/gpio/gpio' + str(pinnum) + '/direction', 'w') as f:
         f.write(str(mode))
@@ -16,12 +21,21 @@ def initpin(pinnum, mode):
 def setpin(pinnum, value):
     with open('/sys/class/gpio/gpio' + str(pinnum) + '/value', 'w') as f:
         f.write(str(value))
-
+def closedpin(pinnum):
+    with open('/sys/class/gpio/gpio' + str(pinnum) + '/value', 'w') as f:
+        f.write(str(0))
+        
 if __name__ == '__main__':
-    initpin(75, 'out')
+    initpin(157, 'out')
+    a=0
     while True:
-        setpin(75, 0)
-        time.sleep(1)
-        setpin(75, 1)
-        time.sleep(1)
-
+        a+=1
+        if a<5:
+            setpin(157, 0)
+            time.sleep(1)
+            setpin(157, 1)
+            time.sleep(1)
+            print(a)
+        else:
+            closedpin(157)
+            break
